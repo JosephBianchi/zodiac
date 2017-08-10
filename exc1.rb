0da2faffc4141448ba6e826cf65c01040d909b57
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-10 17:05:30 -0400

    finished exc 1, here's your dang zodiak

--- exc1.rb
+++ exc1.rb
@@ -7,7 +7,16 @@ require 'date'
 zodiak = {
   "aries" => {start_date: "March 21", end_date: "April 19"},
   "Taurus" => {start_date: "April 20", end_date: "May 20"},
-  "Gemini" => {start_date: "May 21", end_date: "June 20"}
+  "Gemini" => {start_date: "May 21", end_date: "June 20"},
+  "Cancer" => {start_date: "June 21", end_date: "July 22"},
+  "Leo" => {start_date: "July 23", end_date: "August 22"},
+  "Virgo" => {start_date: "August 23", end_date: "September 22"},
+  "Libra" => {start_date: "September 23", end_date: "October 22"},
+  "Scorpio" => {start_date: "October 23", end_date: "November 21"},
+  "Sagittarius" => {start_date: "November 22", end_date: "December 21"},
+  "Capricorn" => {start_date: "December 22", end_date: "January 19"},
+  "Aquarius" => {start_date: "January 20", end_date: "February 20"},
+  "Pisces" => {start_date: "February 19", end_date: "March 20"},
 }
 
 
@@ -30,7 +39,7 @@ month = gets.chomp
 
 
 
-puts date_of_birth = DateTime.parse("#{month} #{day}")
+date_of_birth = DateTime.parse("#{month} #{day}")
 
 
 
